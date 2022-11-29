b0584e78f82e5ae57d0586b006274ec1ffe4a3fc
yooseek <kyungseek.yoo2projectwith.io>
2022-11-29 16:20:59 +0900

    - user quiz finish!

--- app/src/main/java/io/kseek/myandroidapp/MainActivity.kt
+++ app/src/main/java/io/kseek/myandroidapp/MainActivity.kt
@@ -1,45 +1,23 @@
 package io.kseek.myandroidapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnNextPage = findViewById<Button>(R.id.customNextBtn)
-        btnNextPage.setOnClickListener {
+        btnNextPage.setOnClickListener{
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
-            finish()
+//            finish()
         }
 
     }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    fun clickDatePicker() {
-        val myCalendar = Calendar.getInstance()
-        val year = myCalendar.get(Calendar.YEAR)
-        val month = myCalendar.get(Calendar.MONTH)
-        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Year $year, month ${month +1}, day $dayOfMonth", Toast.LENGTH_LONG).show()
-
-                val td = SimpleDateFormat("dd/MM/yyyy", Locale.KOREAN).parse("$dayOfMonth/${month+1}/$year")
-                println(td)
-
-            },
-            year,
-            month,
-            day
-        ).show()
-
-        Toast.makeText(this, "btnDatePrick pressed", Toast.LENGTH_LONG).show()
-    }
-
 }
--- app/src/main/java/io/kseek/myandroidapp/SecondActivity.kt
+++ app/src/main/java/io/kseek/myandroidapp/SecondActivity.kt
@@ -1,15 +1,131 @@
 package io.kseek.myandroidapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var currentUser = 1
+    private var currentPosition = 0
+    private var userList : ArrayList<User>? = null
+
+    private var nextBtn : Button? = null
+    private var secondMainText : TextView? = null
+    private var userOption1: TextView? = null
+    private var userOption2: TextView? = null
+    private var userOption3: TextView? = null
+    private var userOption4: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val userList = Constants.getUsers()
-        Log.i("User size is", "${userList.size}")
+        userList = Constants.getUsers()
+        Log.i("User size is", "${userList?.size}")
+
+        nextBtn = findViewById(R.id.nextUser)
+        secondMainText = findViewById(R.id.secondScreen)
+        userOption1 = findViewById(R.id.userOption1)
+        userOption2 = findViewById(R.id.userOption2)
+        userOption3 = findViewById(R.id.userOption3)
+        userOption4 = findViewById(R.id.userOption4)
+
+        viewUser()
+
+        userOption1?.setOnClickListener(this)
+        userOption2?.setOnClickListener(this)
+        userOption3?.setOnClickListener(this)
+        userOption4?.setOnClickListener(this)
+        nextBtn?.setOnClickListener(this)
+    }
+
+    private fun selectedOptionView(user:TextView, selectedOption : Int) {
+        defaultOptionView()
+
+        currentPosition = selectedOption
+
+        user.setTextColor(Color.parseColor("#363A43"))
+        user.setTypeface(user.typeface,Typeface.BOLD)
+        user.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
+    }
+
+    private fun defaultOptionView() {
+        val optionList = ArrayList<TextView>()
+        optionList.add(0, userOption1!!)
+        optionList.add(1, userOption2!!)
+        optionList.add(2, userOption3!!)
+        optionList.add(3, userOption4!!)
+
+        for (option in optionList) {
+            option.setTextColor(Color.BLUE)
+            option.typeface = Typeface.DEFAULT
+            option.background = null
+        }
+    }
+
+    private fun viewUser () {
+        defaultOptionView()
+
+        var user : User = userList!![currentUser - 1]
+
+        secondMainText?.text = user.name
+        userOption1?.text = user.optionOne
+        userOption2?.text = user.optionTwo
+        userOption3?.text = user.optionThree
+        userOption4?.text = user.optionFour
+
+        if(currentUser ==  userList!!.size){
+            nextBtn?.text = "Finish"
+        }else {
+            nextBtn?.text = "next user"
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id){
+            R.id.userOption1 -> {
+                userOption1?.let{
+                    selectedOptionView(it,1)
+                }
+            }
+            R.id.userOption2 -> {
+                userOption2?.let{
+                    selectedOptionView(it,2)
+                }
+            }
+            R.id.userOption3 -> {
+                userOption3?.let{
+                    selectedOptionView(it,3)
+                }
+            }
+            R.id.userOption4 -> {
+                userOption4?.let{
+                    selectedOptionView(it,4)
+                }
+            }
+            R.id.nextUser -> {
+                currentUser++
+                when {
+                    currentUser <= userList!!.size -> {
+                        viewUser()
+
+                        currentPosition = 0
+                    }else -> {
+                        Toast.makeText(this,"Finish the User!!",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 }
